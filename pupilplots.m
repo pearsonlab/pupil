7bfc1f7ce8f5346f8d7b0f988258380745c721d1
aleigh <a.brown@jacobs-university.de>
2013-06-26 15:13:39 -0400

    more editing to pupilplots

--- pupilplots.m
+++ pupilplots.m
@@ -1,14 +1,30 @@
 %simple function to plot pupil points
+%for dark
 
-function [q,k ]=pupilplots (timeStampAll,rightEyeAll, leftEyeAll, StimOff, StimOnSet);
-hold on
 yL = [-1 4];
-k=plot(timeStampAll,rightEyeAll(:,12));
+fig = figure('Name','Pupil During Dark Test'); 
+k=plot(timeStampAll_dark,rightEyeAll_dark(:,12));
 set(k, 'Color', 'm')
-q=plot(timeStampAll,leftEyeAll(:,12));
+hold on
+q=plot(timeStampAll_dark,leftEyeAll_dark(:,12));
 set(q, 'Color', 'b')
 for i=1:length(StimOff)
-line([StimOff(i) StimOff(i)], yL, 'Color', 'r');
-line([StimOnSet(i) StimOnSet(i)], yL, 'Color', 'g');
+line([StimOff_dark(i) StimOff_dark(i)], yL, 'Color', 'r');
+line([StimOnSet_dark(i) StimOnSet_dark(i)], yL, 'Color', 'g');
 end
+% for light
+yL = [-1 4];
+fig = figure('Name','Pupil During Light Test'); 
+k=plot(timeStampAll_light,rightEyeAll_light(:,12));
+set(k, 'Color', 'm')
+hold on
+q=plot(timeStampAll_light,leftEyeAll_light(:,12));
+set(q, 'Color', 'b')
+for i=1:length(StimOff)
+line([StimOff_light(i) StimOff_light(i)], yL, 'Color', 'r');
+line([StimOnSet_light(i) StimOnSet_light(i)], yL, 'Color', 'g');
 end
+
+%for reversal learning
+
+
