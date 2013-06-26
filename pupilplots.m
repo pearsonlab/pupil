ff2a424b4d68157902b620bead3c7ee8cc9698e1
aleigh <a.brown@jacobs-university.de>
2013-06-26 11:01:25 -0400

    More small changes

--- pupilplots.m
+++ pupilplots.m
@@ -1,11 +1,11 @@
 %simple function to plot pupil points
 
-function r=pupilplots (timeStampAll,rightEyeAll, leftEyeAll, StimOff, StimOnSet);
-yL = get(gca,'YLim');
-r=plot(timeStampAll,rightEyeAll(:,12));
-set(r, 'Color', 'c')
-r=plot(timeStampAll,leftEyeAll(:,12));
-set(r, 'Color', 'b')
+function [r,q,k ]=pupilplots (timeStampAll,rightEyeAll, leftEyeAll, StimOff, StimOnSet);
+yL = [-1 4];
+k=plot(timeStampAll,rightEyeAll(:,12));
+set(k, 'Color', 'm')
+q=plot(timeStampAll,leftEyeAll(:,12));
+set(q, 'Color', 'b')
 for i=1:length(StimOff)
 line([StimOff(i) StimOff(i)], yL, 'Color', 'r');
 line([StimOnSet(i) StimOnSet(i)], yL, 'Color', 'g');
