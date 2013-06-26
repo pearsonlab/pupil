2dd3e53204be61b58ff6ec773f16c8964c382276
Anna Fang <anna.p.fang@gmail.com>
2013-06-26 10:46:46 -0400

    Re-adding tetio_CONNECT

---
+++ tetio_CONNECT.m
@@ -0,0 +1,62 @@
+%% CONNECT TO TOBII TRACKER
+%%%% If not working, try again - can be finicky.
+
+addpath('/Applications/tobiiSDK/matlab/EyeTrackingSample');
+addpath('/Applications/tobiiSDK/matlab//tetio');  
+addpath('/matlab/pupil');
+% Repushing
+% *************************************************************************
+%
+% Initialization and connection to the Tobii Eye-tracker
+%
+% *************************************************************************
+ 
+disp('Initializing tetio...');
+tetio_init();
+
+% Set to tracker ID to the product ID of the tracker you want to connect to.
+trackerId = 'TT060-301-14200895';
+
+%   FUNCTION "SEARCH FOR TRACKERS" IF NOTSET
+if (strcmp(trackerId, 'NOTSET'));
+	warning('tetio_matlab:EyeTracking', 'Variable trackerId has not been set.'); 
+	disp('Browsing for trackers...');
+
+	trackerinfo = tetio_getTrackers();
+	for i = 1:size(trackerinfo,2);
+		disp(trackerinfo(i).ProductId);
+	end
+
+	tetio_cleanUp();
+	error('Error: the variable trackerId has not been set. Edit the EyeTrackingSample.m script and replace "NOTSET" with your tracker id (should be in the list above) before running this script again.');
+end
+
+fprintf('Connecting to tracker "%s"...\n', trackerId);
+tetio_connectTracker(trackerId);
+	
+currentFrameRate = tetio_getFrameRate;
+fprintf('Frame rate: %d Hz.\n', currentFrameRate);
+
+
+%% sync. the clocks
+
+WaitSecs(3);
+
+
+tetio_localToRemoteTime(tetio_localTimeNow())
+
+%tetio_localTimeNow()
+
+%WaitSecs(1)
+
+%%look for synchronization
+
+tetio_clockSyncState;
+
+if tetio_clockSyncState == 0
+    warning('tetio_clocksyncfalse' , 'Clocks are not synchronized');
+    disp('Clocks are not synchronized');
+end
+
+
+
