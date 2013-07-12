b715036e5f075a84c04196647ae6fcfc1a1ddab6
aleigh <a.brown@jacobs-university.de>
2013-07-12 14:41:44 -0400

    Making clean plot take the start of the positions of the event times

--- cleanplot.m
+++ cleanplot.m
@@ -13,16 +13,29 @@ eyedata.righteye(eyedata.righteye(:,13)~=0,12) = NaN;
 
 %get timestamp into a signed time so that it can be used
 eyedata.timestamp=int64(eyedata.timestamp);
+%eyedata.timestamp=(eyedata.timestamp-(datamat(1,3))/1000000
 
 %create a matrix with left, right, and timestamp
 datamat(:,1)=eyedata.lefteye(:,12);
 datamat(:,2)=eyedata.righteye(:,12);
 datamat(:,3)=eyedata.timestamp;
 
+chopmat=[];
+%%this spits out the bins that contain the position of each onset of the
+%%stimulus
+for ind = 1:length(data)
+for i=1:length(datamat)
+d(i)=data(ind).ontime-(datamat(i,3));
+end
+[n onbin(ind)]=min(abs(d));
+chopmat(:,ind)=datamat((onbin(ind)-60):(onbin(ind)+120),1)
+end
+
+
 
 % switch type
 %     case oddball
-data(find(trialvec(1,:)==1)) %find the trials where an oddball occured
+%data(find(trialvec(1,:)==1)) %find the trials where an oddball occured
 
 
 plot(datamat(:,3),datamat(:,1));
