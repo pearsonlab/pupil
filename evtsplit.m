d8b602bb260cea08ac449d57ffcb5ea19ce40f58
aleigh <a.brown@jacobs-university.de>
2013-07-18 15:44:02 -0400

    events is a script to get the relevant events for each type, evtsplit is being edited to work with our code

--- evtsplit.m
+++ evtsplit.m
@@ -1,9 +1,11 @@
-function [outdat,time] = evtsplit(data,evt,pretime,posttime,sr,starttime)
+function [outdat,time] = evtsplit(evt,pretime,posttime,sr,starttime)
 %splits the time series data in data with sampling rate sr into a matrix of
 %snippets with one row for each event timestamp in evt; pretime and
 %posttime are the times prior to and following evt to grab; time is a list
 %of times for each bin relative to the entries in evt
 %starttime is the timestamp of the first bin in data
+
+
 
 %define frequency if it isn't supplied
 if ~exist('sr','var')
@@ -14,28 +16,18 @@ if ~exist('starttime','var')
     starttime = 0;
 end
 
+
 numevt=numel(evt); %number of event timestamps
 dt = 1/sr; %time bin size
 npre = ceil(pretime*sr); %number of bins to grab before
 npost = ceil(posttime*sr); %number of bins to grab after
 
+%kdata=data;
 evtrel = evt - starttime;
 
-outdat = nan(numevt,npre+npost+1); %npre+npost+0 bin
 for ind = 1:numevt
-    bins_to_grab = (-npre:npost) + round( evtrel(ind)/dt );
-    
-    %now take care of ends of time series
-    if bins_to_grab(1) < 1  %if we're at the start of series...
-        bins_to_grab = bins_to_grab(bins_to_grab >= 1); %truncate
-        outdat(ind,(end-length(bins_to_grab)+1):end) = data(bins_to_grab);
-    elseif bins_to_grab(end) > length(data) %if we're at the end...
-        bins_to_grab = bins_to_grab(bins_to_grab <= length(data)); %truncate
-        outdat(ind,1:length(bins_to_grab)) = data(bins_to_grab);
-    else
-        outdat(ind,:) = data(bins_to_grab);
-    end
-    
+    bins_to_grab= evt(ind)+(-npre:npost) 
+    outdat(:,ind)=bins_to_grab
 end
 
 time = (-npre:npost)*dt;
