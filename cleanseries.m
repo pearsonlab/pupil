7232271c4545b3172c85575dc8edca9da66fe0bd
aleigh <a.brown@jacobs-university.de>
2013-07-24 11:33:28 -0400

    slight edits to cleanseries so it works with eye data in its current form

--- cleanseries.m
+++ cleanseries.m
@@ -1,7 +1,9 @@
 function out = cleanseries(data)
 % performs data cleaning (replacing bad/missing data with nan) for tobii
 % gaze data
-data = data(:); %columnify
+%data = data(:); %columnify
+
+data=[data.lefteye(:,12);data.righteye(:,12)] %
 
 N = length(data);
 bad = (data == -1); %points with invalid data
