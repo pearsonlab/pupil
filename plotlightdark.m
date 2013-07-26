bc0fab4f999c6731f6d2b9c640de5d6c4df021ca
aleigh <a.brown@jacobs-university.de>
2013-07-26 08:47:01 -0400

    Making light test plot 4 lines

--- plotlightdark.m
+++ plotlightdark.m
@@ -1,4 +1,4 @@
-function plotlightdark(srtbins,testdata,chopmat)
+function plotlightdark(srtbins,testdata,chopmat,task)
 
 for i = 1:length(srtbins)
     line([srtbins(i) srtbins(i)],[min(testdata.lefteye(:,12)-0.25) max(testdata.lefteye(:,12)+0.25)]);
@@ -7,6 +7,9 @@ end
 % hleg1 = legend('First Stimulus','Second Stimulus', 'Third Stimulus');
 
 figure;
+if strcmp(task, 'darktest')==1
 plot(chopmat(:,1:3));
+else
+    plot(chopmat(:,1:4))
 
 end
