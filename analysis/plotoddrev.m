3ca86fe0779dc2d36d9e2f14f4cb73ebd1024088
Anna Fang <anna.p.fang@gmail.com>
2013-08-05 09:16:10 -0400

    Old changes to plotting functions

--- analysis/plotoddrev.m
+++ analysis/plotoddrev.m
@@ -13,13 +13,14 @@ sizen = size(normavg);
 for i = 1:length(trialvec)
     if trialvec(i) == 0
         %line([srtbins(i) srtbins(i)],[min(eyedata.lefteye(:,12)-0.25) max(eyedata.lefteye(:,12)+0.25)]);
-            %line([srtbins(i) srtbins(i)],[0 5]);
+            x = line([srtbins(i) srtbins(i)],[0 5]);
+            set(x, 'Color', 'k','linewidth',2);
 
     else
         %y = line([srtbins(i) srtbins(i)],[min(eyedata.lefteye(:,12)-0.25) max(eyedata.lefteye(:,12)+0.25)]);
-                y = line([srtbins(i) srtbins(i)],[2 5]);
+               % y = line([srtbins(i) srtbins(i)],[2 5]);
 
-        set(y, 'Color', 'k','linewidth',2);
+        %set(y, 'Color', 'k','linewidth',2);
     end
 end
 
@@ -31,10 +32,10 @@ oddavg(oddavg == 0) = NaN;
 for i=1:length(normavg)
     plot2odd(i,2) = nanmean(normavg(i,:));
     plot2odd(i,1) = nanmean(oddavg(i,:));
-    plot2odd(i,3) = plot2odd(i,1)-nanstd(normavg(i,:))/sizen(2);
-    plot2odd(i,4) = plot2odd(i,1)+nanstd(normavg(i,:))/sizen(2);
-    plot2odd(i,5) = plot2odd(i,2)-nanstd(oddavg(i,:))/sizeo(2);
-    plot2odd(i,6) = plot2odd(i,2)+nanstd(oddavg(i,:))/sizeo(2);
+    plot2odd(i,3) = plot2odd(i,1)-nanstd(normavg(i,:))/sqrt(sizen(2));
+    plot2odd(i,4) = plot2odd(i,1)+nanstd(normavg(i,:))/sqrt(sizen(2));
+    plot2odd(i,5) = plot2odd(i,2)-nanstd(oddavg(i,:))/sqrt(sizeo(2));
+    plot2odd(i,6) = plot2odd(i,2)+nanstd(oddavg(i,:))/sqrt(sizeo(2));
 end
 
 figure;
@@ -44,6 +45,6 @@ plot(plot2odd(:,3:6),'--','Color','k');
     set(gca,'XTick',0:60:length(outdat));
     set(gca,'XTickLabel',{'-1','0','1','2','3'});
     set(gca,'FontSize',14);
-    line([60 60],[-0.4 0.4],'Color','k','linewidth',2);
+    line([60 60],[-0.4 0.5],'Color','k','linewidth',2);
 
 end
