8ae6024a1f00352a0ab83e9d640ae85844cf59a5
Anna Fang <anna.p.fang@gmail.com>
2013-08-08 13:59:38 -0400

    Changes to precision in convertlums

--- task/convertlums.m
+++ task/convertlums.m
@@ -1,10 +1,15 @@
-function [RBGconv,lumtoRBG] = convertlums(RBGval)
+function [RGBconv] = convertlums(RGBval)
 lumens_max = 118.877; % Max luminance measure value based on Spyder
-RBG_max = 255; % Max RBG value
+RGBval = RGBval;
+RGB_max = 255; % Max RBG value
 
-lumens_real = (lumens_max*RBGval)/RBG_max; % Find lumen value corresponding to % max luminance we want
-% RBGtolum(x) = 0.002*(x^2)-(0.01717*x)+1.3493;
-lumtoRBG = 250*(lumens_real/125 - 104995911/10000000000)^(1/2) + 1717/400; % inverse - convert lumens to RBG
-RBGconv = [lumtoRBG lumtoRBG lumtoRBG];
+lumens_real = RGBval/RGB_max;% Find normalized lumen value corresponding to % max luminance we want
+% RBGtolum(x) = 1.1197508*(x^2) - (0.1538355*x) + 0.0113504
+lumtoRGB = (2251799813685248*((5042909285627815*lumens_real)/1125899906842624 - 35272722328531464996282178689579/1298074214633706907132624082305024)^(1/2))/5042909285627815 + 5542508003810831/80686548570045040; % inverse - convert lumens to RBG
+if lumtoRGB > 1
+    lumtoRGB = 1
+end
+RGBconv = 255*[lumtoRGB lumtoRGB lumtoRGB];
+
 end
 
