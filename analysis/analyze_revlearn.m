5ec37a9cd7ccac52ee94912a2a9819d5098f7c81
John Pearson <jmxpearson@gmail.com>
2013-12-06 15:24:12 -0500

    added code to analyze reversal learning

---
+++ analysis/analyze_revlearn.m
@@ -0,0 +1,30 @@
+% analyze_revlearn.m
+% scratch code for analysis of reversal learning
+
+etimes = us2secs([data.soundtime], t0);
+wascorr = logical([data.correct]);
+tpre = -0.3;
+tpost = 8;
+[pmat, binT] = evtsplit(pupil, etimes, tpre, tpost, sr);
+
+% % separate by pupil following correct vs incorrect responses
+% ppmat(1, :) = nanmean(pmat(wascorr, :));
+% ppmat(2, :) = nanmean(pmat(~wascorr, :));
+% pmat = ppmat;
+% 
+% % baseline normalize
+% pmat = basenorm(pmat, binT, [-inf 0]);
+
+pcorr = basenorm(pmat(wascorr, :), binT, [-inf 0]);
+pinc = basenorm(pmat(~wascorr, :), binT, [-inf 0]);
+
+type = 1;
+plot_with_sem(pcorr, 0, 1/sr, type, binT, [0 1 0])
+plot_with_sem(pinc, 0, 1/sr, type, binT, [1 0 0])
+
+%plot(binT, pmat', 'linewidth', 2.0)
+xlim([tpre tpost])
+title('Pupillary response to negative feedback','fontsize', 20, 'fontweight', 'bold')
+xlabel('Time from buzzer (seconds)', 'fontsize', 16, 'fontweight', 'bold')
+ylabel('Normalized pupil size (arb units)', 'fontsize', 16, 'fontweight', 'bold')
+legend({'Correct', '', 'Incorrect', ''}, 'location', 'southeast')
--- analysis/sample_analysis_scratch.m
+++ analysis/sample_analysis_scratch.m
@@ -22,43 +22,6 @@ switch task
         analyze_darktest
     case 'lighttest'
         analyze_lighttest
+    case 'revlearn'
+        analyze_revlearn
 end
-
-%% load data
-dfile = '0.1.revlearn.pupiltest.mat';
-load(fullfile(ddir, dfile))
-
-%% munge data
-lpup = cleanseries(eyedata.lefteye(:, 12));
-rpup = cleanseries(eyedata.righteye(:, 12));
-mpup = nanmean([lpup; rpup]);
-pupil = mpup;
-
-% get timestamps: convert microseconds to seconds
-t0 = min(eyedata.timestamp);
-taxis = (eyedata.timestamp - t0)/1e6;  
-sr = 60;  % sampling rate of Tobii = 60 Hz
-
-%% try some plotting
-etimes = double(uint64([data.soundtime]) - t0)/1e6;
-wascorr = logical([data.correct]);
-tpre = -0.3;
-tpost = 8;
-[pmat, binT] = evtsplit(pupil, etimes, tpre, tpost, sr);
-
-% separate by pupil following correct vs incorrect responses
-ppmat(1, :) = nanmean(pmat(wascorr, :));
-ppmat(2, :) = nanmean(pmat(~wascorr, :));
-pmat = ppmat;
-
-% baseline normalize
-zbin = find(binT == 0);
-normalizer = nanmean(pmat(:, 1:(zbin - 1)), 2);
-pmat = bsxfun(@minus, pmat, normalizer);
-
-plot(binT, pmat', 'linewidth', 2.0)
-xlim([tpre tpost])
-title('Pupillary response to negative feedback','fontsize', 20, 'fontweight', 'bold')
-xlabel('Time from buzzer (seconds)', 'fontsize', 16, 'fontweight', 'bold')
-ylabel('Normalized pupil size (arb units)', 'fontsize', 16, 'fontweight', 'bold')
-legend({'Correct', 'Incorrect'}, 'location', 'southeast')
