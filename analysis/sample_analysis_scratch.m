68e3b0441c2179c7715c457b068845efdaa3a37f
John Pearson <jmxpearson@gmail.com>
2014-02-21 11:10:27 -0500

    fixed data path

--- analysis/sample_analysis_scratch.m
+++ analysis/sample_analysis_scratch.m
@@ -4,7 +4,7 @@
 addpath('~/code/pupil/analysis')
 addpath('~/code/pupil/analysis/utils')
 addpath('~/code/electrophysiology')
-ddir = '~/Dropbox/pupil/data/';
+ddir = '~/data/pupil';
 
 %% load data
 [dfile, newdir, was_success] = uigetfile([ddir '*.mat']);
